89dd75dbfb74fb05d7d8753396a437d684846dbe
Mikhail <zinchenko@surfstudio.co>
2019-09-16 01:10:02 +0300

    Init project

---
+++ app/src/main/java/com/liarstudio/recycler_drag/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.liarstudio.recycler_drag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
